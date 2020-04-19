c80626450c229cc121872569e7fe3785dba93e3b
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-04-19 12:14:06 -0400

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -4,12 +4,16 @@
 # and will create a symbolic link to the
 # Vengen source file.
 
-git clone https://github.com/binexisHATT/Vengen.git
+git --version 2>&1/dev/null
+git_version_status=$?
 
-sleep 4
-
-cd Vengen
-
-sleep 1
-
-ln ./vengen.sh /bin/vengen
+if [ git_version_status -eq 0 ];
+then
+  git clone https://github.com/binexisHATT/Vengen.git
+  sleep 3
+  cd Vengen
+  sleep 1
+  ln ./src/vengen.sh /bin/vengen
+else
+  echo "Git needs to be installed for this installation... Please install it!"
+fi
