20a35d2e6fdabcf443dee7b49c91d8b088dd667f
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-04-19 12:44:35 -0400

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -10,10 +10,12 @@ git_version_status=$?
 if [ $git_version_status -eq 0 ];
 then
   git clone https://github.com/binexisHATT/Vengen.git
-  sleep 3
+  sleep 2
   cd Vengen
   sleep 1
   ln ./src/vengen.sh /bin/vengen
+  sleep 1
+  rm install.sh
 else
   echo "Git needs to be installed for this installation... Please install it!"
 fi
