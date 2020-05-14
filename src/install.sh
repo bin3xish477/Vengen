394374cbe472c83d44761a818c0ba0017e230761
sixelA zeugirdoR <44281620+binexisHATT@users.noreply.github.com>
2020-05-14 16:21:11 -0400

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -9,11 +9,10 @@ git_version_status=$?
 
 if [ $git_version_status -eq 0 ];
 then
+  cd ~/Downloads
   git clone https://github.com/binexisHATT/Vengen.git
   sleep 2
-  cd Vengen
-  sleep 1
-  sudo ln -s ./src/vengen.sh /bin/vengen
+  sudo ln -s ~/Downloads/Vengen/src/vengen.sh /bin/vengen
   sleep 1
   rm ../install.sh
 else
